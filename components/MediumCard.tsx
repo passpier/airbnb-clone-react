2181c2cba1628f091449e0a7427554f833aa39f7
passpier <poo9810@gmail.com>
2023-07-31 14:58:51 +0800

    add info card

--- components/MediumCard.tsx
+++ components/MediumCard.tsx
@@ -7,12 +7,7 @@ const MediumCard: React.FC<Category> = ({ img, title }) => {
   return (
     <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
       <div className="relative h-80 w-80">
-        <Image
-          src={img}
-          fill
-          alt="accommodation image"
-          className="rounded-xl"
-        />
+        <Image src={img} fill alt="category image" className="rounded-xl" />
       </div>
       <h3 className="text-2xl mt-3">{title}</h3>
     </div>
